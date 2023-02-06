23ee8011e1b8f9c34ecf07bf44cabf5b62cd924e
Samuel Ventura <samuel.ventura@yeico.com>
2023-02-05 18:22:38 -0600

    Setup scripts update.

--- setup-rpi3.sh
+++ setup-rpi3.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -xe
 
+cd ~
+
+#select from outside dpi_firmware
+mix dpi.select rpi3
+
 cd ~/release/dpi_firmware
 
-mix dpi.select rpi3
 mix dpi deps.get
 mix dpi compile
 
--- setup-rpi4.sh
+++ setup-rpi4.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -xe
 
+cd ~
+
+#select from outside dpi_firmware
+mix dpi.select rpi4
+
 cd ~/release/dpi_firmware
 
-mix dpi.select rpi4
 mix dpi deps.get
 mix dpi compile
 
